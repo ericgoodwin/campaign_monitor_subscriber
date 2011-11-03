12314c7dfeb4d45e168aaa67e53633ef30379b19
Eric Goodwin <eric@ericgoodwin.com>
2011-11-02 22:03:56 -0700

    Updated so that I can use the add and remove on an instance as well as after_create and after_destroy

--- lib/campaign_monitor_subscriber.rb
+++ lib/campaign_monitor_subscriber.rb
@@ -1,26 +1,41 @@
 module CampaignMonitorSubscriber
-  require 'campaigning'
-  CM_CONFIG = YAML::load_file(File.join("config/campaign_monitor_subscriber_config.yml"))
-  ::CAMPAIGN_MONITOR_API_KEY = CM_CONFIG['api_key']  
 
-  def subscribe_me_using(email_field, custom_fields={})
-    return if CM_CONFIG[::Rails.env] == false or %w(cucumber test).include? ::Rails.env
-    after_create do |record|
-      begin
-        custom_fields = custom_fields.inject({}) { |h, (k, v)| h[k] = record.send(v); h }
-        s = Campaigning::Subscriber.new(record.send(email_field), custom_fields["name"])
-        s.add!(CM_CONFIG['list_id'], custom_fields)
-      rescue RuntimeError
-      end
-    end
-
-    after_destroy do |record|
-      begin
-        Campaigning::Subscriber.unsubscribe!(record.send(email_field), CM_CONFIG['list_id'])
-      rescue RuntimeError
-      end
-    end
+  def self.included(base)
+    base.extend ClassMethods  
   end
+
+  module ClassMethods
+    def belongs_to_mailinglist
+      
+      include CampaignMonitorSubscriber::InstanceMethods
+      
+      after_create  :add_to_mailinglist
+      after_destroy :remove_from_mailinglist
+    end
+
+  end
+  
+  module InstanceMethods
+    
+    def add_to_mailinglist
+      return unless email.present?
+      begin
+        s = Campaigning::Subscriber.new email, login
+        s.add! ::CAMPAIGN_MONITOR_LIST_ID
+      rescue
+      end
+    end
+    
+    def remove_from_mailinglist
+      return unless email.present?
+      begin
+        Campaigning::Subscriber.unsubscribe! email, ::CAMPAIGN_MONITOR_LIST_ID
+      rescue
+      end
+    end
+    
+  end
+  
 end
 
 ActiveRecord::Base.extend(CampaignMonitorSubscriber)
